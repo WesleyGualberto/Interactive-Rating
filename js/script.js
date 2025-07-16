4c807e472e8fa20ac58e33c4bee9e6fec19f74c6
Wesley Gualberto <37596981+WesleyGualberto@users.noreply.github.com>
2025-07-16 07:01:03 -0300

    Aprimorado CSS e adicionado JS 👍
    Foi aprimorado os estilos no CSS e adicionado JS com função para armazenar o voto e mostrar na tela escondendo a tela de voto e mostrando a tela de agradecimento pela respostas além do alerta caso tente enviar sem ter selecionado a nota! 💯

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var botao = document.querySelector(".botao");
+var resposta = document.querySelector("#youSelected");
+var divFormulario = document.querySelector(".interactive:not(.thankyou)");
+var divAgradecimento = document.querySelector(".thankyou");
+
+botao.addEventListener("click", function() {
+    event.preventDefault();
+
+    const notaSelecionada = document.querySelector("input[name='rating']:checked");
+
+    if (notaSelecionada) {
+        const valor = notaSelecionada.value;
+        resposta.textContent = "You selected " + valor + " out of 5";
+
+        divFormulario.classList.add("escondido")
+
+        divAgradecimento.classList.remove("escondido")
+    } else {
+        window.alert("Escolha uma nota para registrar seu voto!");
+    }
+})
